7fd1b6d29a67731d7f9915dbfd7390da99553326
EgleSva <lazegle@gmail.com>
2023-12-13 13:28:53 +0200

    Scraping done

--- main2Analysis.py
+++ main2Analysis.py
@@ -1,11 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import psycopg2
 
-# # Load data from CSV
-df = pd.read_csv("imdbfilms.csv")
+# Duomenų bazės prisijungimo parametrai
+db_host = 'localhost'
+db_name = 'films'
+db_user = 'postgres'
+db_password = ''
 
-### Apsivalome duomenis
+# Prisijungiame prie duomenų bazės
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}/{db_name}')
 
+# SQL užklausa duomenims ištraukti
+select_query = '''
+    SELECT title, genre, years, duration, rating, people_rating, critic_rating, votes
+    FROM imdbfilms
+'''
+
+# Sukuriame DataFrame iš SQL duomenų
+df = pd.read_sql_query(select_query, engine)
+
+# Peržiūrime duomenis
+# print(df.head())
+
+# Apsivalome duomenis
 df.dropna(axis=0, inplace=True)
 df['years'] = pd.to_numeric(df['years'], errors='coerce')
 df['people_rating'] = df['people_rating'].astype(float)
@@ -13,32 +32,31 @@ df['critic_rating'] = df['critic_rating'].astype(int)
 df['duration'] = df['duration'].astype(int)
 df['votes'] = df['votes'].replace(',', '', regex=True)
 df['votes'] = df['votes'].astype(int)
-
-## pasitikriname, kokius duomenu tipus turime
-column_types = df.dtypes
-# print(column_types)
-
-### Zmoniu ir kritiku reitingai per metus
-avg_ratings_by_year = df.groupby('years')[['people_rating', 'critic_rating']].mean()
-
-fig, ax1 = plt.subplots(figsize=(12, 6))
-ax1.plot(avg_ratings_by_year.index, avg_ratings_by_year['people_rating'], label='People Rating', marker='o', color='green')
-ax1.set_xlabel('Year')
-ax1.set_ylabel('People Rating', color='green')
-ax1.tick_params(axis='y', labelcolor='green')
-ax1.set_ylim(1, 10)
-ax2 = ax1.twinx()
-ax2.plot(avg_ratings_by_year.index, avg_ratings_by_year['critic_rating'], label='Critic Rating', marker='o', color='red')
-ax2.set_ylabel('Critic Rating', color='red')
-ax2.tick_params(axis='y', labelcolor='red')
-ax2.set_ylim(1, 100)
-plt.title('Average Ratings by Year')
-# plt.show()
-
-### Top 10 filmu, kurie turi vidutiniskai geriausius ivertinimus, atsizvelgiant i zmoniu balsavima ir kritiku balsavima
+df['title'] = df['title'].astype("string")
 
 
-### Top 5 populiariausia filmu metai
+# Top 5 populiariausia filmu metai
 populiariausi_metai = df['years'].value_counts().head(5)
 # print(populiariausi_metai)
 
+# Random pavadinimas - top 5 dazniausiai naudojami zodziai pavadinimuose, kurie yra ilgesni arba lygu nei 4 raides, pavadinimuose
+col = 'title'
+df_filtered = df[df[col].notnull()]
+all_words = ' '.join(df_filtered[col].astype(str))
+# print(all_words)
+words = all_words.split()
+filtered_words = [word for word in words if len(word) >= 4]
+
+word_counts = {}
+for word in filtered_words:
+    word_counts[word] = filtered_words.count(word)
+
+top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:5]
+top_words_df = pd.DataFrame(top_words, columns=['Word', 'Frequency'])
+# print(top_words_df)
+
+plt.bar(top_words_df['Word'], top_words_df['Frequency'])
+plt.xlabel('Words')
+plt.ylabel('Frequency')
+plt.title('Top 5 words in titles that are longer or equal to 4 letters')
+plt.show()
