6d21d9532b5572fbf4af07b004f54a88d2ea466b
EgleSva <lazegle@gmail.com>
2023-12-13 14:09:13 +0200

    Scraping done

--- main2Analysis.py
+++ main2Analysis.py
@@ -40,39 +40,39 @@ df['title'] = df['title'].astype("string")
 populiariausi_metai = df['years'].value_counts().head(5)
 # print(populiariausi_metai)
 
-# 5 lentele. Random pavadinimas - top 5 dazniausiai naudojami zodziai pavadinimuose, kurie yra ilgesni arba lygu nei 4 raides, pavadinimuose
-col = 'title'
-
-# Ismetame eilutes su null reiksme
-df_filtered = df[df[col].notnull()]
-
-# visus pavadinimus sujungiame i viena eilute
-all_words = ' '.join(df_filtered[col].astype(str))
-# print(all_words)
-
-# atskiriame visus zodziu kaip atskirus
-words = all_words.split()
-
-# issifiltruojam tik tuos zodziu, kruie turi 4 ar daugiau raidziu
-filtered_words = [word for word in words if len(word) >= 4]
-
-# susikuriam zodziu ir ju pasikartojimu skaiciu dictionary
-word_counts = {}
-for word in filtered_words:
-    word_counts[word] = filtered_words.count(word)
-
-# dictionary pasiverciame i tuple, tada pagal antraji elementa tuple (daznis) issrikiuojam sarasa, sarasa rasaome mazejancia tvarka ir paimam top 5
-top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:5]
-top_words_df = pd.DataFrame(top_words, columns=['Word', 'Frequency'])
-# print(top_words_df)
-
-# priesiame grafika
-plt.bar(top_words_df['Word'], top_words_df['Frequency'])
-plt.xlabel('Words')
-plt.ylabel('Frequency')
-plt.title('Top 5 words in titles that are longer or equal to 4 letters')
-plt.show()
-# 5 lentele pabaiga.
+# # 5 lentele. Random pavadinimas - top 5 dazniausiai naudojami zodziai pavadinimuose, kurie yra ilgesni arba lygu nei 4 raides, pavadinimuose
+# col = 'title'
+#
+# # Ismetame eilutes su null reiksme
+# df_filtered = df[df[col].notnull()]
+#
+# # visus pavadinimus sujungiame i viena eilute
+# all_words = ' '.join(df_filtered[col].astype(str))
+# # print(all_words)
+#
+# # atskiriame visus zodziu kaip atskirus
+# words = all_words.split()
+#
+# # issifiltruojam tik tuos zodziu, kruie turi 4 ar daugiau raidziu
+# filtered_words = [word for word in words if len(word) >= 4]
+#
+# # susikuriam zodziu ir ju pasikartojimu skaiciu dictionary
+# word_counts = {}
+# for word in filtered_words:
+#     word_counts[word] = filtered_words.count(word)
+#
+# # dictionary pasiverciame i tuple, tada pagal antraji elementa tuple (daznis) issrikiuojam sarasa, sarasa rasaome mazejancia tvarka ir paimam top 5
+# top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:5]
+# top_words_df = pd.DataFrame(top_words, columns=['Word', 'Frequency'])
+# # print(top_words_df)
+#
+# # priesiame grafika
+# plt.bar(top_words_df['Word'], top_words_df['Frequency'])
+# plt.xlabel('Words')
+# plt.ylabel('Frequency')
+# plt.title('Top 5 words in titles that are longer or equal to 4 letters')
+# plt.show()
+# # 5 lentele pabaiga.
 
 
 # # 1 lentele.  Koreliacija - kaip kinta zmoniu ivertinimas, nuo balsu kiekio
@@ -89,49 +89,54 @@ plt.show()
 # # 1 lentele pabaiga.
 
 
-# 6 lentele. Penktadienio filmo rekomendacija - Top 10, geriausias ivertinimas pagal kritikus, geriausias ivertinimas pagal zmones, daugiausia balsu
-# sortinam top10
-top10_movies_critic = df.sort_values(by='critic_rating', ascending=False).head(10)
+# # 6 lentele. Penktadienio filmo rekomendacija - Top 10 filmu, kuriu pavadinime yra zodis Christmas, geriausias ivertinimas pagal kritikus, geriausias ivertinimas pagal zmones, daugiausia balsu.
+# # Filtruojame filmus, kurių pavadinime yra "Christmas"
+# df_christmas = df[df['title'].str.contains('Christmas', case=False)]
+#
+# # Rikiuojame top10 pagal critic_rating
+# top10_movies_critic = df_christmas.sort_values(by='critic_rating', ascending=False).head(10)
+#
+# # Votes to numeric
+# top10_movies_critic['votes'] = top10_movies_critic['votes'].apply(lambda x: pd.to_numeric(x, errors='coerce'))
+#
+# # dydis
+# fig, ax1 = plt.subplots(figsize=(12, 8))
+#
+# # pirma asis
+# color = 'tab:blue'
+# ax1.set_xlabel('Title')
+# ax1.set_ylabel('Critic rating', color=color)
+# ax1.bar(top10_movies_critic['title'], top10_movies_critic['critic_rating'], label='Critic Rating', color=color)
+# ax1.tick_params(axis='y', labelcolor=color)
+#
+# # antra asis
+# ax2 = ax1.twinx()
+# color = 'tab:red'
+# ax2.set_ylabel('People rating', color=color)
+# ax2.plot(top10_movies_critic['title'], top10_movies_critic['people_rating'], label='People Rating', marker='o', color=color)
+# ax2.tick_params(axis='y', labelcolor=color)
+#
+# # trecia asis
+# ax3 = ax1.twinx()
+# color = 'tab:green'
+# ax3.spines['right'].set_position(('outward', 60))
+# ax3.set_ylabel('Votes', color=color)
+# ax3.plot(top10_movies_critic['title'], top10_movies_critic['votes'], label='Votes', marker='s', color=color)
+# ax3.tick_params(axis='y', labelcolor=color)
+# ax3.set_xticks(ax1.get_xticks())  # Pridėta eilutė
+#
+# # Pridėta eilutė, kad nustatytumėte ašies ribas nuo 0 iki didžiausios votes reikšmės
+# max_votes = top10_movies_critic['votes'].max()
+# ax3.set_ylim(0, max_votes)
+#
+# # pasukimas
+# ax1.set_xticklabels(top10_movies_critic['title'], rotation=45, ha='right')
+#
+# # aprasymas
+# fig.suptitle('Top 10 Critically Acclaimed Movies with People Ratings and Votes')
+# fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+# plt.show()
+# # 6 lentele pabaiga.
 
-# Votes to numeric
-top10_movies_critic['votes'] = top10_movies_critic['votes'].apply(lambda x: pd.to_numeric(x, errors='coerce'))
 
-# dydis
-fig, ax1 = plt.subplots(figsize=(12, 8))
-
-# pirma asis
-color = 'tab:blue'
-ax1.set_xlabel('Title')
-ax1.set_ylabel('Critic rating', color=color)
-ax1.bar(top10_movies_critic['title'], top10_movies_critic['critic_rating'], label='Critic Rating', color=color)
-ax1.tick_params(axis='y', labelcolor=color)
-
-# antra asis
-ax2 = ax1.twinx()
-color = 'tab:red'
-ax2.set_ylabel('People rating', color=color)
-ax2.plot(top10_movies_critic['title'], top10_movies_critic['people_rating'], label='People Rating', marker='o', color=color)
-ax2.tick_params(axis='y', labelcolor=color)
-
-# trecia asis
-ax3 = ax1.twinx()
-color = 'tab:green'
-ax3.spines['right'].set_position(('outward', 60))
-ax3.set_ylabel('Votes', color=color)
-ax3.plot(top10_movies_critic['title'], top10_movies_critic['votes'], label='Votes', marker='s', color=color)
-ax3.tick_params(axis='y', labelcolor=color)
-ax3.set_xticks(ax1.get_xticks())  # Pridėta eilutė
-
-# Pridėta eilutė, kad nustatytumėte ašies ribas nuo 0 iki didžiausios votes reikšmės
-max_votes = top10_movies_critic['votes'].max()
-ax3.set_ylim(0, max_votes)
-
-# pasukimas
-ax1.set_xticklabels(top10_movies_critic['title'], rotation=45, ha='right')
-
-# aprasymas
-fig.suptitle('Top 10 Critically Acclaimed Movies with People Ratings and Votes')
-fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-plt.show()
-# 6 lentele pabaiga.
-
+# 4 lentele. Populiariausias zanras pagal metus, metus imant nuo 2000
